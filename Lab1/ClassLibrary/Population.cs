c4c576fdf5e402413222642149cb31418d59659f
simalinna <simalinna@mail.ru>
2024-09-23 12:31:37 +0300

    init commit

---
+++ Lab1/ClassLibrary/Population.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Population
+    {
+        public List<Chromosome> routes { get; set; }
+
+        public int citiesCount { get; set; }
+
+        public int populationSize { get; set; }
+
+        public Population(int populationSize, double[,] distances)
+        {
+            this.populationSize = populationSize;
+
+            citiesCount = distances.GetLength(0);
+
+            this.routes = new List<Chromosome> ();
+            createPopulation(populationSize, distances);
+
+            sortPopulation(routes);
+        }
+
+        public string populationToString()
+        {
+            string res = "";
+            foreach (var item in routes)
+            {
+                res+=item.chromosomeToString();
+            }
+            return res;
+        }
+
+        public string bestRoutesToString()
+        {
+            string res = "";
+            foreach (var item in getBestRoutes())
+            {
+                item.route.ForEach(elem => res += $"{elem} -> ");
+                res += $"{item.route[0]}\n";
+            }
+            return res;
+        }
+
+        public void evolution(double[,] distances)
+        {
+            Random random = new Random();
+
+            List<Chromosome> newRoutes = routes.ToList();
+
+            foreach (var mutant in routes)
+                if (random.Next(101) <= 30)
+                    newRoutes.Add(new Chromosome(mutant.route, citiesCount, distances));
+
+            int parent1Index = random.Next(populationSize); ;
+            int parent2Index = random.Next(populationSize); ;
+            while (parent1Index == parent2Index)
+            {
+                parent1Index = random.Next(populationSize);
+                parent2Index = random.Next(populationSize);
+            }
+
+            newRoutes.Add(new Chromosome(routes[parent1Index].route, routes[parent2Index].route, citiesCount, distances));
+
+            sortPopulation(newRoutes);
+            newRoutes.RemoveRange(populationSize, newRoutes.Count - populationSize);
+
+            routes = newRoutes;
+        }
+
+        public void createPopulation(int populationSize, double[,] distances)
+        {
+            for (int i = 0; i < populationSize; i++)
+            {
+                routes.Add(new Chromosome(citiesCount, distances));
+            }
+        }
+
+        public void sortPopulation(List<Chromosome> routes)
+        {
+            routes.Sort((b1, b2) => b1.fitness.CompareTo(b2.fitness));
+        }
+
+        public double getBestDistance()
+        {
+            return routes.Min(elem => elem.fitness);
+        }
+
+        public double getMeanDistance()
+        {
+            return routes.Average(elem => elem.fitness);
+        }
+
+        public List<Chromosome> getBestRoutes()
+        {
+;           double bestFitness = getBestDistance();
+            List<Chromosome> bestRoutes = new List<Chromosome>();
+            foreach (var item in routes)
+                if (item.fitness == bestFitness)
+                    bestRoutes.Add(item);
+            return bestRoutes;
+        }       
+
+    }
+}
+
+
---
+++ Lab1/Lab1/Program.cs
@@ -0,0 +1,69 @@
+﻿using ClassLibrary;
+using System.Runtime.InteropServices;
+
+
+int POPULATION_SIZE = 10;
+//int MAX_GENERATIONS = 10;
+
+
+// 1 -> 3 -> 5 -> 2 -> 4 -> 1
+// 15
+double[,] distances =
+{
+    {0, 6, 1, 5, 10 },
+    {6, 0, 7, 4, 3 },
+    {1, 7, 0, 9, 2 },
+    {5, 4, 9, 0, 8 },
+    {10, 3, 2, 8, 0 },
+};
+
+// 1 -> 4 -> 2 -> 6 -> 3 -> 5 -> 1
+// 21
+double[,] distances_1 =
+{
+    {0, 8, 11, 1, 6, 7 },
+    {8, 0, 9, 2, 12, 3 },
+    {11, 9, 0, 10, 5, 4 },
+    {1, 2, 10, 0, 11, 9 },
+    {6, 12, 5, 11, 0, 12 },
+    {7, 3, 4, 9, 12, 0 },
+};
+
+Population myPopulation = new Population(POPULATION_SIZE, distances);
+
+
+//Console.CancelKeyPress += (sender, args) =>
+//{
+//    args.Cancel = true;
+//    //Console.WriteLine(args.SpecialKey);
+//    flag = false;
+//};
+
+
+ConsoleKeyInfo cki;
+
+int generationsCount = 1;
+
+//for (int i = 0; i < MAX_GENERATIONS; i++)
+while (true)
+{
+    Console.WriteLine($"Generation: {generationsCount++}\n");
+
+    //Console.Write(myPopulation.populationToString());
+
+    Console.WriteLine($"Best distance: {myPopulation.getBestDistance()}");
+    Console.WriteLine($"Mean distance : {myPopulation.getMeanDistance()}\n");
+
+    Console.WriteLine($"Best routes:\n");
+
+    Console.Write(myPopulation.bestRoutesToString());
+
+    Console.WriteLine($"---------------------------------------------------------------");
+
+    //System.Threading.Thread.Sleep(1000);
+
+    cki = Console.ReadKey(true);
+    if (cki.Key == ConsoleKey.Spacebar) break;
+
+    myPopulation.evolution(distances);
+}
