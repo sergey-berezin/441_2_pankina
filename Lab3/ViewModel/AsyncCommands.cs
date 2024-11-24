23c931697cc5d16d8bc871f589c2e11e62e56d9c
simalinna <simalinna@mail.ru>
2024-11-24 23:24:02 +0300

    init commit

---
+++ Lab3/ViewModel/AsyncCommands.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+    public class AsyncCommands: ICommand
+    {
+        private readonly Func<object, bool> canExecute;
+        private readonly Func<object, Task> executeAsync;
+        private bool isExecuting;
+
+        public AsyncCommands(Func<object, Task> executeAsync, Func<object, bool> canExecute = null)
+        {
+            this.executeAsync = executeAsync;
+            this.canExecute = canExecute;
+        }
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            if (isExecuting)
+            {
+                return false;
+            }
+            else
+            {
+                return canExecute is null || canExecute(parameter);
+            }
+        }
+
+        public void Execute(object? parameter)
+        {
+            if (!isExecuting)
+            {
+                isExecuting = true;
+                executeAsync(parameter).ContinueWith(_ =>
+                {
+                    isExecuting = false;
+                    CommandManager.InvalidateRequerySuggested();
+                }, scheduler: TaskScheduler.FromCurrentSynchronizationContext());
+            }
+        }
+    }
+}
